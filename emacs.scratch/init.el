fdfbf8df62d53fa8b4b2e32a7e984e417f2bc4e3
Fran_ <fkrausear@gmail.com>
2021-09-23 23:23:02 -0300

    Add magit, projectile and tresitter

--- emacs.scratch/init.el
+++ emacs.scratch/init.el
@@ -1,12 +1,15 @@
 ;; You will most likely need to adjust this font size for your system!
 
+
+;; Less typing == Happines
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
 (tooltip-mode -1)           ; Disable tooltips
 (set-fringe-mode 10)        ; Give some breathing room
 
 (menu-bar-mode -1)            ; Disable the menu bar
-
 ;; Set up the visible bell
 (setq visible-bell nil)
 
@@ -20,7 +23,7 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
+                        ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
@@ -30,25 +33,18 @@
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
    (package-install 'use-package))
-
 (require 'use-package)
+
 (setq use-package-always-ensure t)
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
-(setq display-line-numbers 'relative)
-
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
 (defun toggle-lines ()
   (interactive)
   (if (eq display-line-numbers 'relative)
       (setq display-line-numbers t)
       (setq display-line-numbers 'relative)))
 
-;; A fancy dashboard for Emacs's startup
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook))
 ;; Disable line numbers for some modes
 (dolist (mode '(term-mode-hook
                 shell-mode-hook
@@ -74,7 +70,7 @@
          ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
-(setq ivy-height 15)
+(setq ivy-height 20)
 (global-set-key (kbd "C-M-b") 'counsel-switch-buffer)
 ;; NOTE: The first time you load your configuration on a new machine, you'll
 ;; need to run the following command interactively so that mode line icons
@@ -83,6 +79,8 @@
 ;; M-x all-the-icons-install-fonts
 
 (use-package all-the-icons)
+(use-package all-the-icons-ivy
+  :init (add-hook 'after-init-hook 'all-the-icons-ivy-setup))
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
@@ -99,6 +97,7 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.5))
+(which-key-setup-minibuffer)
 
 (use-package ivy-rich
   :init
@@ -123,23 +122,6 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-(use-package general)
-
-(general-define-key
-  "C-M-b" 'counsel-switch-buffer)
-
-(general-create-definer general//leader-key
-:keymaps '(normal insert visual emacs)
-:prefix "SPC"
- :global-prefix "C-SPC")
-
-(general//leader-key
- "f" '(:ignore t :which-key "Files")
- "ff" '(counsel-find-file :which-key "Open a file")
- "d" '(:ignore t :which-key "Dired")
- "dj" '(dired-jump :which-key "Open dired on the current dir.")
- "b" '(:ignore t :which-key "Buffers")
- "bs" '(counsel-switch-buffer :which-key "Switch buffers"))
 
 
 (defun evil//evil-hook()
@@ -161,9 +143,7 @@
   (setq evil-want-C-u-delete t)
   :hook (evil-mode . evil//evil-hook)
   :config
-  (progn
-    (evil-mode 1)
-    (turn-on-evil-mode))
+  (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
@@ -173,11 +153,15 @@
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
+(evil-mode 1)
+(turn-on-evil-mode)
 
 (use-package evil-collection
   :after evil
   :config
   (evil-collection-init))
+(use-package evil-commentary
+  :after evil-collection)
 
 (use-package hydra)
 
@@ -186,9 +170,72 @@
   ("j" text-scale-increase "in")
   ("k" text-scale-decrease "out")
   ("f" nil "finished" :exit t))
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-switch-project-action #'projectile-dired))
+(use-package counsel-projectile
+  :after projectile
+  :config (counsel-projectile-mode))
 
 ;;(rune/leader-keys
 ;;  "ts" '(hydra-text-scale/body :which-key "scale text"))
+(use-package magit
+  :commands (magit-status magit-get-current-branch))
+(use-package forge)
+(use-package general)
+
+(general-define-key
+  "C-M-b" 'counsel-switch-buffer)
+
+(general-create-definer general//leader-key
+:keymaps '(normal insert visual emacs)
+:prefix "SPC"
+ :global-prefix "C-SPC")
+
+(general//leader-key
+  "g" '(:ignore t :which-key "Magit...")
+  "gg" '(magit-status :which-key "Status")
+  "gc" '(magit-checkout :which-key "Checkout")
+  "gb" '(magit-branch-checkout :which-key "Branch Checkout")
+  "gf" '(magit-file-checkout :which-key "File Checkout")
+ "e" '(:ignore t :which-key "Eval...")
+ "el" '(eval-last-sexp :which-key "Last sexp")
+ "eb" '(eval-buffer :which-key "Buffer")
+ "f" '(:ignore t :which-key "Files")
+ "ff" '(counsel-find-file :which-key "Open a file")
+ "fr" '(counsel-recentf :which-key "Open a recent file")
+ "d" '(:ignore t :which-key "Dired")
+ "dp" '(projectile-dired :which-key "Dired Current Project")
+ "dj" '(dired-jump :which-key "Open dired on the current dir.")
+ "b" '(:ignore t :which-key "Buffers")
+ "bs" '(counsel-switch-buffer :which-key "Switch buffers")
+ "br" '(revert-buffer-quick :which-key "Revert buffer")
+ "s" '(:ignore t :which-key "Search")
+ "ss" '(swiper  :which-key "Search Buffer")
+ "sp" '(counsel-projectile-ag :which-key "Search Current Project")
+ "p" '(:ignore t :which-key "Projectile")
+ "pr" '(projectile-run-project :which-key "Run Project")
+ "pc" '(projectile-compile-project :which-key "Switch project")
+ "pp" '(counsel-projectile-switch-project :which-key "Switch project")
+ "pf" '(counsel-projectile-find-file :which-key "Open Project File ")
+ "pa"'(projectile-add-known-project :which-key "Add project"))
+
+
+(use-package tree-sitter)
+(use-package tree-sitter-langs
+  :after tree-sitter)
+(progn
+(global-tree-sitter-mode)
+(add-hook 'prog-mode-hook
+	    #'(lambda ()
+		(tree-sitter-hl-mode))))
+  
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -196,7 +243,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard hydra evil-collection evil general helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline all-the-icons ivy command-log-mode use-package)))
+   '(tree-sitter-langs all-the-icons-ivy evil-commentary evil-commentary-mode forge evil-magit magit counsel-projectile projectile which-key use-package rainbow-delimiters ivy-rich hydra helpful general evil-collection doom-themes doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
