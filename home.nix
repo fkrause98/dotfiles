bf993047d6d6a44b4f7ef87c4ff8af149a2a8bd2
Francisco Krause <fkrausear@gmail.com>
2023-04-13 02:08:27 -0300

    Update some fish and vim config

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@ let
   isMac = builtins.currentSystem == "darwin";
   isLinux = builtins.currentSystem == "x86_64-linux";
   iterm = if isMac then [ pkgs.iterm2 ] else [ ];
-  elixir-ls = if isMac then [pkgs.elixir-ls] else [];
+  elixir-ls = if isMac then [ pkgs.elixir-ls ] else [ ];
   home = builtins.getEnv "HOME";
   user = builtins.getEnv "USER";
 in {
@@ -21,48 +21,53 @@ in {
   home.stateVersion = "22.11"; # Please read the comment before changing.
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [
-    asdf-vm
-    ripgrep
-    fd
-    exa
-    bat
-    tmux
-    direnv
-    rustup
-    gnumake
-    neofetch
-    jq
-    nixfmt
-    neovim
-    plocate
-    eb-garamond
-    # DOOM Emacs dependencies
-    binutils
-    (ripgrep.override { withPCRE2 = true; })
-    gnutls
-    fd
-    imagemagick
-    zstd
-    nodePackages.javascript-typescript-langserver
-    sqlite
-    editorconfig-core-c
-    libvterm
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  home.packages = with pkgs;
+    [
+      asdf-vm
+      ripgrep
+      fd
+      exa
+      bat
+      tmux
+      direnv
+      rustup
+      gnumake
+      neofetch
+      jq
+      nixfmt
+      neovim
+      plocate
+      eb-garamond
+      # DOOM Emacs dependencies
+      binutils
+      (ripgrep.override { withPCRE2 = true; })
+      gnutls
+      fd
+      imagemagick
+      zstd
+      nodePackages.javascript-typescript-langserver
+      sqlite
+      editorconfig-core-c
+      libvterm
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    (writeShellScriptBin "install-emacs" ''
-    ${if isMac then "brew install emacs &&"  else ""} + 
-    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-    ~/.config/emacs/bin/doom install
-    '')
-  ] ++ iterm ++ elixir-ls;
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      (writeShellScriptBin "install-emacs" ''
+        ${if isMac then "brew install emacs &&" else ""} + 
+        git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+        ~/.config/emacs/bin/doom install
+      '')
+      (writeShellScriptBin "prompt-setup" ''
+        !#/usr/bin/env fish
+        fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
+      '')
+    ] ++ iterm ++ elixir-ls;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -103,44 +108,44 @@ in {
     # The emacs aarch64 is currently broken, so
     # ignore it for now, plus I'd rather
     # use railwaycat's port.
-    enable = true;
+    enable = isLinux;
     package = pkgs.emacs-gtk;
-    extraPackages = ( epkgs: [ epkgs.vterm ] );
+    extraPackages = (epkgs: [ epkgs.vterm ]);
   };
   # Tmux config
   programs.tmux = {
     enable = true;
     extraConfig = ''
-         # List of plugins
-         set -g @plugin 'tmux-plugins/tpm'
-         set -g @plugin 'tmux-plugins/tmux-sensible'
-         set -g @plugin 'jabirali/tmux-tilish'
-         set-window-option -g mode-keys vi
-         # Keybinds
-         bind-key -T copy-mode-vi v send -X begin-selection
-         bind-key -T copy-mode-vi V send -X select-line
-         bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
-         unbind -T copy-mode MouseDragEnd1Pane
-         unbind -T copy-mode-vi MouseDragEnd1Pane
-         bind-key -T copy-mode-vi y send-keys -X copy-selection
-         bind-key -T vi-copy    MouseDragEnd1Pane copy-selection -x
-         bind-key -T copy-mode-vi y send -X copy-selection-no-clear
-         set-option -g mouse on
-         setw -g mode-mouse on
-         set -g terminal-overrides 'xterm*:smcup@:rmcup@'
-         # Other examples:
-         # set -g @plugin 'github_username/plugin_name'
-         # set -g @plugin 'github_username/plugin_name#branch'
-         # set -g @plugin 'git@github.com:user/plugin'
-         # set -g @plugin 'git@bitbucket.com:user/plugin'
-         set-option -g status-position top
-         # Start windows and panes at 1, not 0
-         set -g base-index 1
-         setw -g pane-base-index 1
+      # List of plugins
+      set -g @plugin 'tmux-plugins/tpm'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
+      set -g @plugin 'jabirali/tmux-tilish'
+      set-window-option -g mode-keys vi
+      # Keybinds
+      bind-key -T copy-mode-vi v send -X begin-selection
+      bind-key -T copy-mode-vi V send -X select-line
+      bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
+      unbind -T copy-mode MouseDragEnd1Pane
+      unbind -T copy-mode-vi MouseDragEnd1Pane
+      bind-key -T copy-mode-vi y send-keys -X copy-selection
+      bind-key -T vi-copy    MouseDragEnd1Pane copy-selection -x
+      bind-key -T copy-mode-vi y send -X copy-selection-no-clear
+      set-option -g mouse on
+      setw -g mode-mouse on
+      set -g terminal-overrides 'xterm*:smcup@:rmcup@'
+      # Other examples:
+      # set -g @plugin 'github_username/plugin_name'
+      # set -g @plugin 'github_username/plugin_name#branch'
+      # set -g @plugin 'git@github.com:user/plugin'
+      # set -g @plugin 'git@bitbucket.com:user/plugin'
+      set-option -g status-position top
+      # Start windows and panes at 1, not 0
+      set -g base-index 1
+      setw -g pane-base-index 1
 
-         # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-         run '~/.tmux/plugins/tpm/tpm'
-         '';
+      # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+      run '~/.tmux/plugins/tpm/tpm'
+    '';
   };
   # Vim  config
   programs.vim = {
@@ -154,10 +159,28 @@ in {
        set tabstop=2
        set softtabstop=2
        set shiftwidth=2
+       " Remember last position on file
+      if has("autocmd")
+        augroup redhat
+        autocmd!
+        " When editing a file, always jump to the last cursor position
+        autocmd BufReadPost *
+          \ if line("'\"") > 0 && line ("'\"") <= line("$") |
+          \   exe "normal! g'\"" |
+          \ endif
+        augroup END
+      endif
     '';
   };
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      ## Alias ls to exa
+      function ls --wraps=exa --description 'alias ls exa'
+      exa $argv
+      end
+
+    '';
     plugins = with pkgs.fishPlugins; [
       {
         name = "z";
@@ -175,9 +198,31 @@ in {
         name = "fzf";
         src = fzf.src;
       }
+      {
+        name = "tide";
+        src = tide.src;
+      }
+      {
+        name = "done";
+        src = done.src;
+      }
+      {
+        name = "fish-git-abbr";
+        src = pkgs.fetchFromGitHub {
+          owner = "lewisacidic";
+          repo = "fish-git-abbr";
+          rev = "abe95203b7fcb1eaa685bd5b75796b52bb7be884";
+          sha256 = "UP+bk5luk6bNrMzI4eQkt9TzU8dkjEqGoEnlEh4fEK4=";
+        };
+      }
       { 
-        name = "pure";
-        src = pure.src;
+        name = "fish-abbreviation-tips";
+        src = pkgs.fetchFromGitHub {
+          owner = "gazorby";
+          repo = "fish-abbreviation-tips";
+          rev = "8ed76a62bb044ba4ad8e3e6832640178880df485";
+          sha256 = "F1t81VliD+v6WEWqj1c1ehFBXzqLyumx5vV46s/FZRU=";
+        };
       }
       {
         name = "fisher";
