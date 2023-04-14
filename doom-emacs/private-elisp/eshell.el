dc17f8c4a3d1dbe1bf10c24dea12ef14b1f0556e
Francisco Krause Arnim <fkrausear@gmail.com>
2023-04-14 11:56:19 -0300

    Wrap eshell config in an after! block

--- doom-emacs/private-elisp/eshell.el
+++ doom-emacs/private-elisp/eshell.el
@@ -1,11 +1,23 @@
 ;;; ../emacs-doom-config/private-elisp/eshell.el -*- lexical-binding: t; -*-
 
 ;;; Eshell
-(eshell-vterm-mode)
-(defalias 'eshell/v 'eshell-vterm-exec-visual)
-(setq eshell-up-ignore-case nil)
-(add-hook 'eshell-mode-hook
-          #'(lambda ()
-              (company-mode -1)
-              (esh-autosuggest-mode)))
-(setq eshell-term-name "xterm-256color")
+(after! eshell
+  (eshell-vterm-mode)
+  (defalias 'eshell/v 'eshell-vterm-exec-visual)
+  (setq eshell-up-ignore-case nil)
+  (setq eshell-term-name "xterm-256color")
+  ;; Redefine the eshell prompt function
+  ;; to add a new line before the PS1.
+  (defun +eshell-default-prompt-fn ()
+    "Generate the prompt string for eshell. Use for `eshell-prompt-function'."
+    (require 'shrink-path)
+    (concat (if (bobp) "" "\n")
+            (let ((pwd (eshell/pwd)))
+              (propertize (if (equal pwd "~")
+                              pwd
+                            (abbreviate-file-name (shrink-path-file pwd)))
+                          'face '+eshell-prompt-pwd))
+            (propertize (+eshell--current-git-branch)
+                        'face '+eshell-prompt-git-branch)
+            (propertize "\n λ" 'face (if (zerop eshell-last-command-status) 'success 'error))
+            " ")))
