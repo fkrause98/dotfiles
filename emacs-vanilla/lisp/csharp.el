82d3d86d0217c1667e887326d2a5e3218c67787d
fkrause98 <fkrausear@gmail.com>
2023-06-29 00:21:43 -0300

    Emacs: Make csharp hook a proper fun

--- emacs-vanilla/lisp/csharp.el
+++ emacs-vanilla/lisp/csharp.el
@@ -4,11 +4,13 @@
   :straight t
   :mode "\\.cs\\'"
   :init
-  (add-hook 'csharp-mode-hook 'csharp-tree-sitter-mode)
-  (add-hook 'csharp-tree-sitter-mode
-            #'(lambda ()
-                (tree-sitter-indent-mode -1)
-                (setq-local lsp-auto-guess-root t)
-                (lsp)))
+  (add-hook 'csharp-mode-hook 'cshar/mode-hook)
   :hook
   ((csharp-mode) . tree-sitter-hl-mode))
+
+;;;###autoload
+(defun csharp/mode-hook ()
+  (csharp-tree-sitter-mode)
+  (tree-sitter-indent-mode -1)
+  (setq-local lsp-auto-guess-root t)
+  (lsp))
