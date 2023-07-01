726152e3e10f6da8855294fe13dd99e16eec7421
fkrause98 <fkrausear@gmail.com>
2023-07-01 00:35:39 -0300

    Emacs: Move blamer to git.el

--- emacs-vanilla/lisp/git.el
+++ emacs-vanilla/lisp/git.el
@@ -5,3 +5,15 @@
   :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
   :config
   (setq magit-save-repository-buffers 'dontask))
+(use-package blamer
+  :straight t
+  :commands (blamer-mode global-blamer-mode)
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                    :background nil
+                    :height 140
+                    :italic t))))
--- emacs-vanilla/lisp/packages.el
+++ emacs-vanilla/lisp/packages.el
@@ -83,19 +83,6 @@
   :commands (sudo-edit sudo-edit-find-file)
   :defines (sudo-edit sudo-edit-find-file)
   :straight t)
-
-(use-package blamer
-  :straight t
-  :commands (blamer-mode global-blamer-mode)
-  :defer 20
-  :custom
-  (blamer-idle-time 0.3)
-  (blamer-min-offset 70)
-  :custom-face
-  (blamer-face ((t :foreground "#7a88cf"
-                    :background nil
-                    :height 140
-                    :italic t))))
 (use-package fish-mode
   :straight t
   :mode ("\\.fish\\'" . fish-mode))
