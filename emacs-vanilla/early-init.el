9d918347f8a0c3e3bb680ce965870cae581bbf65
fkrause98 <fkrausear@gmail.com>
2023-07-01 01:29:57 -0300

    Emacs: Update elixir-mode

--- emacs-vanilla/early-init.el
+++ emacs-vanilla/early-init.el
@@ -1,2 +1,8 @@
 ;; disable package.el to use straight instead
 (setq package-enable-at-startup nil)
+;;; Move this awful file somewhere else
+(setq custom-file
+      (concat user-emacs-directory "~/.emacs/.custom.el"))
+;;; Move backup files to another folder,
+;;; instead of creating them everywhere
+(setq backup-directory-alist `(("." . "~/.saves")))
--- emacs-vanilla/lisp/elixir.el
+++ emacs-vanilla/lisp/elixir.el
@@ -3,3 +3,18 @@
 (use-package elixir-mode
   :mode (("\\.ex\\'" . elixir-mode)
 	 ("\\.exs\\'" . elixir-mode)))
+
+(use-package mix
+  :after elixir-mode
+  :hook (elixir-mode 'mix-minor-mode)
+  :config
+  (setq compilation-scroll-output 'first-error))
+
+(with-eval-after-load "keys"
+  (keys/local-leader
+    :keymaps 'elixir-mode-map
+    "m" '(:ignore t :which-key "Mix")
+    "f" '(elixir-format :which-key "Format")
+    "mt" '(:ignore t :which-key "Test")
+    "tt" '(mix-test-current-test :which-key "At point")
+    "tb" '(mix-test-current-buffer :which-key "Current buffer")))
