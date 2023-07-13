a70c1306916db93f365cb03b867833a270c744eb
fkrause98 <fkrausear@gmail.com>
2023-07-12 22:03:10 -0300

    Emacs: Enable dashboard

--- emacs-vanilla/lisp/ui.el
+++ emacs-vanilla/lisp/ui.el
@@ -2,7 +2,18 @@
   :straight t)
 ;; Center text when only 1 window
 ;; is open
+
 (use-package perfect-margin
   :hook (after-init . perfect-margin-mode)
   :config
   (perfect-margin-mode 1))
+
+(use-package dashboard
+  :straight t
+  :init
+  (setq dashboard-startup-banner 'logo
+        dashboard-center-content t
+        dashboard-items '((projects . 10)
+                          (recents . 10)))
+  (add-hook 'dashboard-after-initialize-hook 'dashboard-jump-to-projects)
+  (dashboard-setup-startup-hook))
