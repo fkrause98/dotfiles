f20489f544739e2665e098013bb5adb64e9cf3a5
fkrause98 <fkrausear@gmail.com>
2023-07-12 22:03:19 -0300

    Emacs: Enable vertico-prescient

--- emacs-vanilla/lisp/prescient.el
+++ emacs-vanilla/lisp/prescient.el
@@ -4,8 +4,16 @@
   :straight t
   :config
   (prescient-persist-mode 1))
+
+(use-package vertico-prescient
+  :straight t
+  :after (prescient)
+  :config
+  (vertico-prescient-mode))
+
 (use-package company-prescient
   :straight t
   :after (company)
   :config
   (company-prescient-mode 1))
+
