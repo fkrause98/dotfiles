d2fd1c4c49b2ac8637b18349f24926a9334eaa9e
Francisco Krause Arnim <fkrausear@gmail.com>
2023-08-08 20:30:21 -0300

    Emacs: Fix mac config

--- emacs-vanilla/lisp/mac.el
+++ emacs-vanilla/lisp/mac.el
@@ -1,4 +1,4 @@
-;;; package --- Summary: Macos specific settings
+;;; mac.el -*- lexical-binding: t; -*-
 ;;; Get env from shell
 (use-package exec-path-from-shell
   :defer t
@@ -6,3 +6,26 @@
   :init
   (when (memq window-system '(mac ns x))
 	(exec-path-from-shell-initialize)))
+;;; Shell to use
+(setq shell-file-name "fish"
+      vterm-shell "~/.nix-profile/bin/fish")
+;;; Elixir
+(setq elixir-ls-folder "~/elixir-ls")
+;;; Modifiers
+;; One thing that mac does well is
+;; the command key, let's use it.
+(setq mac-command-modifier 'control)
+;; (setq mac-option-modifier 'meta)
+(setq mac-right-option-modifier 'meta)
+(setq mac-pass-command-to-system t)
+(exec-path-from-shell-initialize)
+;; (unless (boundp 'server-running-p)
+;;     (server-start))
+
+(defun funcs//insert-at-char ()
+  (interactive)
+  (insert 64))
+
+;; (map! :i  "M-q" 'funcs//insert-at-char)
+(display-battery-mode)
+;; (push "~/.authinfo" auth-sources)
