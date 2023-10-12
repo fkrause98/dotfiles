e7d73342b273fba7c0434342bd3d20bb0a2a113d
Francisco Krause Arnim <fkrausear@gmail.com>
2023-10-12 00:22:59 -0300

    Emacs: Add some comments for used packages

--- emacs-vanilla/lisp/packages.el
+++ emacs-vanilla/lisp/packages.el
@@ -1,7 +1,11 @@
 ;;; package -- Some non-related (between them) packages
+
+;;; Colorful brackets.
 (use-package rainbow-delimiters
   :straight t
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;;; Upgrade for helpful buffers.
 (use-package helpful
   :straight t
   :after consult
@@ -14,6 +18,7 @@
   ([remap describe-variable] . consult-describe-variable)
   ([remap describe-key] . helpful-key))
 
+;;; Smart bracket matching.
 (use-package smartparens
   :straight t
   :hook (prog-mode . smartparens-mode)
@@ -26,22 +31,26 @@
   (sp-pair "(" nil :post-handlers '(("||\n[i]" "RET"))))
 
 
+;;; Compile + Linting Checks.
 (use-package flycheck
   :hook (lsp-mode . flycheck-mode)
   :config
   (global-flycheck-mode))
 
+
+;;; Complex undo and redo functionality.
 (setq undo-tree-auto-save-history t
       undo-tree-history-directory-alist '(("." . "~/.emacs-undo")))
-
 (use-package undo-tree
   :after evil-mode
   :commands (turn-on-undo-tree-mode global-undo-tree-mode undo-tree-mode))
+
+;;; File explorer
 (use-package neotree
   :straight t
   :after projectile)
 
-
+;;; Indentation lines
 (use-package highlight-indent-guides
   :straight t
   ;; :hook
@@ -52,61 +61,84 @@
   :config
   (setq highlight-indent-guides-method 'character))
 
-;; Despite what its name implies,
-;; it also works on wayland and mac.
+;;; Emacs <-> System clipboard.
+;;; Despite what its name implies,
+;;; it also works on wayland and mac.
 (use-package xclip
   :straight t
   :commands (xclip-mode)
   :hook (after-init . xclip-mode))
 
+;;; Edit files as sudo.
 (use-package sudo-edit
   :commands (sudo-edit sudo-edit-find-file)
   :defines (sudo-edit sudo-edit-find-file)
   :straight t)
 
+;;; Fish-shell files mode.
 (use-package fish-mode
   :straight t
   :mode ("\\.fish\\'" . fish-mode))
 
+;;; Direnv + Emacs integration.
 (use-package direnv
   :after projectile
   :straight t
   :config
   (setq direnv-always-show-summary nil)
   (direnv-mode))
+
+;;; Get proper $PATH on Mac.
 (use-package exec-path-from-shell
   :if *is-mac*
   :straight t
   :config
   (exec-path-from-shell-initialize))
 
+;;; Keep source code always indented. 
 (use-package aggressive-indent
   :straight t
   :commands (aggressive-indent-mode))
 
-(use-package ranger
-  :commands (ranger)
-  :config
-  (with-eval-after-load "keys"
-    (keys/leader
-      "od" '(ranger :which-key "Dired")))
-  (setq ranger-dont-show-binary t
-        ranger-show-literal nil))
 
+;;; Write grep buffers
 (use-package wgrep
   :commands (wgrep-change-to-wgrep-mode)
   :config
   (setq wgrep-change-readonly-file t))
 
-
-
+;;; Yul language support.
 (use-package yul-mode
   :mode
   (("\\.yul\\'" . yul-mode))
   :config
   (add-hook 'yul-mode-hook
-            (lambda ()
+            '(lambda ()
               (setq indent-line-function 'indent-relative))))
 
+;;; Determine indent of file
+(use-package dtrt-indent
+  :after yul-mode
+  :config
+  (add-hook 'yul-mode
+            '(lambda ()
+              (dtrt-indent-mode))))
+
+;;; Toggle string cases
 (use-package string-inflection
-  :commands (string-inflection-java-style-cycle))
+  :commands (string-inflection-java-style-cycle
+             string-inflection-toggle))
+
+;;; Docker support
+(use-package docker
+  :commands (docker))
+
+;;; Avoid that emacs creates
+;;; trash all over the place.
+(use-package no-littering) 
+
+;;; Some more colors for dired
+(use-package diredfl
+  :hook dired-mode
+  :commands diredfl-mode)
+
