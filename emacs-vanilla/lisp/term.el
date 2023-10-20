071688e5750efb7d6bae7ce809354fbc82625896
Francisco Krause Arnim <fkrausear@gmail.com>
2023-10-20 19:19:43 -0300

    Emacs: Add multi-vterm, update some vterm settings

--- emacs-vanilla/lisp/term.el
+++ emacs-vanilla/lisp/term.el
@@ -5,23 +5,11 @@
   :config
   (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *")
   (setq vterm-shell "fish")
-  (setq vterm-max-scrollback 10000))
-
-(use-package vterm-toggle
-  :straight t
-  :commands vterm-toggle
-  :requires vterm)
-
-(use-package multi-vterm
-  :straight t
-  :commands (multi-vterm)
-	:config
+  (setq vterm-max-scrollback 10000)
 	(add-hook 'vterm-mode-hook
-			(lambda ()
-			(setq-local evil-insert-state-cursor 'box)
+			#'(lambda ()
 			(evil-insert-state)))
 	(define-key vterm-mode-map [return]                      #'vterm-send-return)
-
 	(setq vterm-keymap-exceptions nil)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-e")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-f")      #'vterm--self-insert)
@@ -30,7 +18,6 @@
 	(evil-define-key 'insert vterm-mode-map (kbd "C-b")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-w")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-u")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-d")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-n")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-m")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-p")      #'vterm--self-insert)
@@ -41,13 +28,24 @@
 	(evil-define-key 'insert vterm-mode-map (kbd "C-g")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-c")      #'vterm--self-insert)
 	(evil-define-key 'insert vterm-mode-map (kbd "C-SPC")    #'vterm--self-insert)
-	(evil-define-key 'normal vterm-mode-map (kbd "C-d")      #'vterm--self-insert)
+	;; (evil-define-key 'normal vterm-mode-map (kbd "C-d")      #'vterm--self-insert)
 	(evil-define-key 'normal vterm-mode-map (kbd ",c")       #'multi-vterm)
 	(evil-define-key 'normal vterm-mode-map (kbd ",n")       #'multi-vterm-next)
 	(evil-define-key 'normal vterm-mode-map (kbd ",p")       #'multi-vterm-prev)
 	(evil-define-key 'normal vterm-mode-map (kbd "i")        #'evil-insert-resume)
 	(evil-define-key 'normal vterm-mode-map (kbd "o")        #'evil-insert-resume)
-	(evil-define-key 'normal vterm-mode-map (kbd "<return>") #'evil-insert-resume))
+	(evil-define-key 'normal vterm-mode-map (kbd "<return>") #'evil-insert-resume)
+  (evil-define-key 'normal vterm-mode-map (kbd "C-d") 'evil-scroll-down))
+
+(use-package vterm-toggle
+  :straight t
+  :commands vterm-toggle
+  :requires vterm)
+
+(use-package multi-vterm
+  :straight t
+  :commands (multi-vterm)
+	:config)
 
 (use-package eat
   :straight t
