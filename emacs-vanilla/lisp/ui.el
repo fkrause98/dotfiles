bf7feeb6abd064824a123355ff33c5b1764835b2
Francisco Krause Arnim <fkrausear@gmail.com>
2023-10-28 15:36:36 -0300

    Emacs: Replace doom-modeline with mini-echo

--- emacs-vanilla/lisp/ui.el
+++ emacs-vanilla/lisp/ui.el
@@ -32,20 +32,22 @@
      :states 'normal                                 
      "q" 'evil/record-macro-or-kill-popper-buffer)))
 
-(use-package doom-themes)
+;;;###autoload
+(defun funs/set-theme ()
+  (load-theme 'doom-tokyo-night))
+
+(use-package doom-themes
+  :hook (after-init . funs/set-theme))
 
 (use-package color-theme-sanityinc-tomorrow)
 
-(use-package doom-modeline
-  :straight t
-  :config
-  (setq doom-modeline-icon nil)
-  (setq doom-themes-enable-bold nil)
-  (doom-modeline-mode 1))
+;; (use-package doom-modeline
+;;   :straight t
+;;   :config
+;;   (setq doom-modeline-icon nil)
+;;   (setq doom-themes-enable-bold nil)
+;;   (doom-modeline-mode 1))
 
-(add-hook 'after-init-hook
-          #'(lambda ()
-             (load-theme 'doom-tokyo-night)))
 
 ;; Center text when only 1 window
 ;; is open
@@ -79,7 +81,15 @@
 (use-package all-the-icons
   :if (display-graphic-p))
 
+(use-package all-the-icons-completion
+  :hook (after-init . all-the-icons-completion-mode)
+  :if (display-graphic-p)
+  :straight (:host github
+                   :repo "iyefrat/all-the-icons-completion"
+                   :commit "8eb3e410d63f5d0657b41829e7898793e81f31c0"))
+
 (use-package all-the-icons-dired
+  :after (all-the-icons)
   :if (display-graphic-p)
   :hook (dired-mode . all-the-icons-dired-mode))
 
@@ -88,3 +98,18 @@
   (if *is-mac*
       (set-face-attribute 'default nil :family font-to-use)
     (message (format "Missing font: %s" font-to-use))))
+
+(use-package mini-echo
+  :commands (mini-echo-mode)
+  :hook (after-init . mini-echo-mode)
+  :straight (:host github
+                   :repo "liuyinz/mini-echo.el"
+                   :commit "a8b30a91986be60e718963e2c78cae3c3a3c3b76")
+  :config
+;; set default segments of long/short style
+(setq mini-echo-default-segments
+  '(:long ("major-mode" "buffer-name" "vcs" "buffer-position"
+           "buffer-size" "flymake" "process" "selection-info"
+           "narrow" "macro" "profiler")
+    :short ("buffer-name-short" "buffer-position" "process"
+            "profiler" "selection-info" "narrow" "macro"))))
