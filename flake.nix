b389adbb400cbe176673e101ba7d789cf13f8095
Fran <fran@Franciscos-MBP.fibertel.com.ar>
2024-08-31 20:11:31 -0300

    v

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -11,14 +11,14 @@
 
   outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }: {
     darwinConfigurations = {
-      "Frans-Mac" = nix-darwin.lib.darwinSystem {
+      "Franciscos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
         modules = [
           ./darwin-configuration.nix
           home-manager.darwinModules.home-manager
           {
             home-manager = {
-              users.fran = import ../home-manager/home.nix;
+              users.fran = import ./home-manager/home.nix;
             };
             users.users.fran.home = "/Users/fran";
           }
