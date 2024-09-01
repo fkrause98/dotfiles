c8d6afd20300fb35441e8f00c7e264a134035888
Fran <fran@fran-mbp.fibertel.com.ar>
2024-08-31 23:08:09 -0300

    nix-darwin: enable tap-to-click

--- flake.nix
+++ flake.nix
@@ -6,24 +6,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-        url = "github:LnL7/nix-darwin";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-	    url = "github:nix-community/home-manager";
-	    inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    pkgs = (import nixpkgs) { allowUnfree = true; };
-	
-    configuration = {pkgs, ... }: {
+    let
+      pkgs = (import nixpkgs) { allowUnfree = true; };
+
+      configuration = { pkgs, ... }: {
         services.nix-daemon.enable = true;
         # Necessary for using flakes on this system.
         nix.settings.experimental-features = "nix-command flakes";
-	nixpkgs.config.allowUnfree = true;
+        nixpkgs.config.allowUnfree = true;
         system.configurationRevision = self.rev or self.dirtyRev or null;
 
         # Used for backwards compatibility. please read the changelog
@@ -36,44 +36,51 @@
 
         # Declare the user that will be running `nix-darwin`.
         users.users.fran = {
-            name = "fran";
-            home = "/Users/fran";
+          name = "fran";
+          home = "/Users/fran";
         };
 
         # Create /etc/zshrc that loads the nix-darwin environment.
-	programs.zsh.enable = true;
-	programs.fish.enable = true;
-	
-	environment.systemPackages = [ pkgs.fastfetch pkgs.vim pkgs.darwin.CF pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
-	homebrew = {
-		enable = true;
-		taps = [];
-		brews = [ "docker" ];
-		casks = [ "rectangle" "tg-pro" "iterm2" ];
-	};
-	security.pam.enableSudoTouchIdAuth = true;
-   };
-  in
-  {
-    darwinConfigurations."fran-mbp" = nix-darwin.lib.darwinSystem {
-      modules = [
-         configuration
-    home-manager.darwinModules.home-manager  {
-	home-manager.useGlobalPkgs = true;
-	home-manager.useUserPackages = true;
-	home-manager.verbose = true;
-	home-manager.users.fran = import ./home-manager/home.nix ;
-	home-manager.extraSpecialArgs = { 
-		vars = rec {
-			 isMac = true;
-			 isLinux = false;
-			 home = "/Users/fran";
-			 emacsConfig = ./doom;
-  			fishNixPath = home + "/.nix-profile/bin/fish";
-    		};
-	};
-      }
-      ];
+        programs.zsh.enable = true;
+        programs.fish.enable = true;
+
+        environment.systemPackages = [
+          pkgs.fastfetch
+          pkgs.vim
+          pkgs.darwin.CF
+          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+        homebrew = {
+          enable = true;
+          taps = [ ];
+          brews = [ "docker" ];
+          casks = [ "rectangle" "tg-pro" "iterm2" ];
+        };
+        security.pam.enableSudoTouchIdAuth = true;
+        # Enable tap to click
+        system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+      };
+    in {
+      darwinConfigurations."fran-mbp" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.fran = import ./home-manager/home.nix;
+            home-manager.extraSpecialArgs = {
+              vars = rec {
+                isMac = true;
+                isLinux = false;
+                home = "/Users/fran";
+                emacsConfig = ./doom;
+                fishNixPath = home + "/.nix-profile/bin/fish";
+              };
+            };
+          }
+        ];
+      };
     };
-  };
 }
